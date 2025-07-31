007afc4d6be843f2dc6199b25f4c19f41be6c3f0
aylin268 <aylin268@ucsb.edu>
2025-07-30 21:59:40 -0700

    Create MyStack.h

---
+++ MyStack.h
@@ -0,0 +1,34 @@
+#ifndef MYSTACK_H
+#define MYSTACK_H
+
+#include "Stack.h"
+#include <stdexcept>
+
+
+//add to the stack 
+//remove from stack 
+//check if its empty 
+
+
+class MyStack: public Stack {
+public:
+	MyStack();
+	~MyStack();
+
+	double pop() override ; //removes and return the top value 
+	void push(double value) override; //adds to top
+	double top() const override; //returns top value
+	bool is_empty() const override;
+
+
+	//linked List 
+	private:
+		struct Node{
+			double value;
+			Node* next;
+		};
+
+		Node* head;
+};
+
+#endif
